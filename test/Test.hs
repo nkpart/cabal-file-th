a6ff1274208bc74e04cb524acdc3ab4f96aaad2e
Nick Partridge <nkpart@gmail.com>
2011-10-19 20:30:13 +1000

    added a good test

--- test/Test.hs
+++ test/Test.hs
@@ -4,6 +4,9 @@ module Main where
 import Distribution.PackageDescription.TH
 
 main = do
-  putStrLn $(packageVariable (pkgVersion . package))
-  putStrLn $(packageVariableFrom "../kit/kit.cabal" (pkgVersion . package))
-
+  putStrLn $ "This package is version " ++ $(packageVariable (pkgVersion . package))
+  let testVersion = $(packageVariableFrom "test/test-version-interp.cabal" (pkgVersion . package))
+  let expectedVersion = "5.5.5"
+  if testVersion /= expectedVersion
+    then error ("Expected " ++ expectedVersion ++ ", read: " ++ testVersion)
+    else putStrLn "Everything went better than expected."
